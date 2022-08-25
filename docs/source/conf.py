01bc80992308003997b1b8c0b07d4a196bd8329a
Paul David Harris <hifret@neomailbox.com>
2022-08-25 10:02:38 +0300

    Remove import burstH2MM from docs/source/conf.py to fix readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ MOCK_MODULES = ['numpy', 'tables','scipy', 'scipy.stats', 'scipy.optimize',
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
     
-import burstH2MM
 
 # -- Project information -----------------------------------------------------
 
